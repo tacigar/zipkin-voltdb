53894257a8a02d9902a3660d692634574c4220e4
Adrian Cole <acole@pivotal.io>
2019-02-01 11:57:28 +0100

    Adds fields needed to export metric points
    satisfies the needs of https://github.com/ExpediaDotCom/haystack-trends/tree/master/span-timeseries-transformer/src/main/scala/com/expedia/www/haystack/trends/transformer

--- storage/src/main/java/zipkin2/storage/voltdb/VoltDBSpanConsumer.java
+++ storage/src/main/java/zipkin2/storage/voltdb/VoltDBSpanConsumer.java
@@ -58,8 +58,9 @@ final class VoltDBSpanConsumer implements SpanConsumer {
     static Call<Void> create(Client client, Span span) {
       byte[] json = SpanBytesEncoder.JSON_V2.encode(span);
       byte[] md5 = MD5.get().digest(json);
-      StoreSpanJson result =
-          new StoreSpanJson(client, span.traceId(), span.id(), span.timestamp(), md5, json);
+      StoreSpanJson result = new StoreSpanJson(client,
+          span.traceId(), span.id(), span.localServiceName(), span.name(), span.timestamp(),
+          span.duration(), span.tags().containsKey("error") ? 1 : 0, md5, json);
       return result.handleError(result);
     }
 
--- storage/src/main/resources/ddl.sql
+++ storage/src/main/resources/ddl.sql
@@ -1,18 +1,23 @@
 CREATE TABLE span
 (
   trace_id VARCHAR(32) NOT NULL,
-  span_id VARCHAR(16) NOT NULL,
-  ts TIMESTAMP,
-  md5 VARBINARY(16) NOT NULL,
-  json VARCHAR NOT NULL,
-  PRIMARY KEY (trace_id, span_id, md5)
+  id VARCHAR(16) NOT NULL,
+  service_name VARCHAR(255), -- The localEndpoint.serviceName field in json
+  name VARCHAR(255),
+  ts TIMESTAMP, -- Derived from the epoch micros timestamp in json
+  duration BIGINT, -- The duration field in json, in microseconds
+  is_error TINYINT NOT NULL, -- 1 when tags.error exists in json or 0 if not
+  md5 VARBINARY(16) NOT NULL, -- MD5 of the json, used to prevent duplicate rows
+  json VARCHAR NOT NULL, -- Potentially incomplete v2 json sent by instrumentation
+  PRIMARY KEY (trace_id, id, md5)
 );
 
--- Partition this table to get parallelism.
+-- Allows procedures to work on a trace as a unit
 PARTITION TABLE span ON COLUMN trace_id;
 
 CREATE PROCEDURE storeSpanJson PARTITION ON TABLE span COLUMN trace_id PARAMETER 0 AS
-  INSERT INTO span (trace_id, span_id, ts, md5, json) VALUES (?, ?, TO_TIMESTAMP(Micros, ?), ?, ?);
+  INSERT INTO span (trace_id, id, service_name, name, ts, duration, is_error, md5, json)
+    VALUES (?, ?, ?, ?, TO_TIMESTAMP(Micros, ?), ?, ?, ?, ?);
 
 CREATE PROCEDURE getSpanJson PARTITION ON TABLE span COLUMN trace_id PARAMETER 0 AS
   SELECT json from span where trace_id = ?;
